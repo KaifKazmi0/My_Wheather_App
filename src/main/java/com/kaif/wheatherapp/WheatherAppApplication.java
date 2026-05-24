b249289defc4423f9f733f1e2267a2b743637fb4
kaifkazmi <sayedkaif784@gmail.com>
2026-05-24 19:31:56 +0530

    created wheather app repo

---
+++ src/main/java/com/kaif/wheatherapp/WheatherAppApplication.java
@@ -0,0 +1,13 @@
+package com.kaif.wheatherapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WheatherAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WheatherAppApplication.class, args);
+    }
+
+}
